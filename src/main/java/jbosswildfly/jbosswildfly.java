814c3d86298cd253807009a4c9a5df6bf0825752
Harpreet Kaur <c0663712@mylambton.ca>
2016-02-03 10:41:26 -0500

    final

--- src/main/java/jbosswildfly/jbosswildfly.java
+++ src/main/java/jbosswildfly/jbosswildfly.java
@@ -11,28 +11,43 @@
 package jbosswildfly;
 import java.sql.*;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class jbosswildfly {
-    private static Connection getConnection() throws SQLException ,ClassNotFoundException{
+    private static Connection getConnection() throws SQLException {
             try{
         Class.forName("com.mysql.jdbc.Driver");
         }
         catch (ClassNotFoundException ex){
-                Logger.getLogger(jbosswildfly.class.getName()).log(Level.SERVER,null, ex)
+                Logger.getLogger(jbosswildfly.class.getName()).log(Level.SEVERE,null, ex);
     }
-    String host = System.out.getenv("OPENSHIFT_MYSQL_DB_HOST");
-    String port = System.out.getenv("OPENSHIFT_MYSQL_DB_PORT");
-    String username = System.out.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
-    String password = System.out.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+    String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+    String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+    String username = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+    String password = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
     String name= "jbosswildfly";
-    String host = System.out.getenv("OPENSHIFT_MYSQL_DB_HOST");
+    String url = "jbosswildfly://" + host + ":" + port + "/" + name;
+    Connection con = DriverManager.getConnection(url,username,password);
+    return con;
 }
-    }
+    
 
-public static String getTAble(){
+public static String getTable(){
 String output ="";
+Connection conn;
 try{
-Connection conn = getConnection();
-ResultSet rs =stmt.executeQuery("")
+ conn = getConnection();
+Statement stmt =conn.createStatement();//
+ResultSet rs =stmt.executeQuery("select*from people");
+while (rs.next()){
+    output +="<h2>"+rs.getString("name")+"</h2>";
+    output +="<h2>"+rs.getString("bio")+"</h2>";
+}
+
+}
+catch (SQLException ex){
+    output=" SQL exception" +ex.getMessage();
+}
+return output;
 }
 }
